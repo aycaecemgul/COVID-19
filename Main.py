a780e7f593895d06a770438bfb1a5eed3958eafa
aycaecemgul <aycaecemgul@gmail.com>
2021-04-04 17:41:13 +0300

    update

--- Main.py
+++ Main.py
@@ -32,6 +32,8 @@ X = pickle.load(pickle_in)
 pickle_in = open("y.pickle","rb")
 y = pickle.load(pickle_in)
 
+print("Pickle jar opened.")
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,shuffle= True)
 X_train, X_val, y_train, y_val  = train_test_split(X_train, y_train, test_size=0.33, shuffle= True )
 
@@ -43,7 +45,6 @@ pickle_out = open("y_test.pickle","wb")
 pickle.dump(y_test, pickle_out)
 pickle_out.close()
 
-print("Pickle jar opened.")
 
 base_model = InceptionV3(
                                 weights='imagenet',
@@ -60,10 +61,21 @@ model.add(Dense(3, activation='softmax'))
 
 model.summary()
 
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('val_accuracy')>0.7):
+      print("\nReached 70% val accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
 model.compile(loss="sparse_categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
 
 aug = ImageDataGenerator()
+
 BS=150
-model.fit(aug.flow(X_train, y_train, batch_size = BS),validation_data = (X_val, y_val),epochs = 10,verbose=2)
+model.fit(aug.flow(X_train, y_train, batch_size = BS),validation_data = (X_val, y_val),epochs = 10,callbacks=[callbacks],verbose=2)
+
+
 
 model.save("covid-19.model")
